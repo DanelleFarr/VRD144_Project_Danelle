f42884e19da9d7b1ab9214fbb2b7d5e8ebe0e47e
Danelle Farrington <farringtond774@students.cochise.edu>
2020-10-01 11:48:53 -0700

    Fixed colliders for music and instrument stands

---
+++ Assets/ReturnToStartPos.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class ReturnToStartPos : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    Vector3 startPos;
+    [SerializeField]
+    Vector3 startRot;
+    void Start()
+    {
+        //set the start position and rotation based on where the object starts.
+        startPos = this.transform.position;
+        startRot = this.transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   //reset the object to it's starting position and rotation
+    public void ReturnToStart()
+    {
+        this.transform.position = startPos;
+        this.transform.eulerAngles = startRot; 
+    }//end ReturnToStart()
+}
---
+++ Assets/TriggerControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Events;
+using UnityEngine.Events;
+
+public class TriggerControls : MonoBehaviour
+{
+    //Fields
+    [SerializeField]
+    UnityEvent TriggerEnter;
+    [SerializeField]
+    UnityEvent TriggerExit;
+    [SerializeField]
+    UnityEvent TriggerStay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerExit.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerExit.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerStay.Invoke();
+    }
+}
