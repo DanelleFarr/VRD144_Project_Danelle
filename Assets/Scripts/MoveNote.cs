443f70c4f973260d80ff36c316b60f3344f5d82f
Danelle Farrington <farringtond774@students.cochise.edu>
2020-10-05 14:39:24 -0700

    fixed improper light and audio calls in switches, added starter note with Movement and ReturnToStart upon collision

---
+++ Assets/Scripts/MoveNote.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MoveNote : MonoBehaviour
+{
+    [SerializeField]
+    float speed = .01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveForward();
+    }
+
+    private void MoveForward()
+    {
+        this.transform.Translate(speed, 0, 0, Space.World);
+    }
+}
