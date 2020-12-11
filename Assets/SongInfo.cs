c779ba843f2caf343d9fe956d7a772828d3dac60
Danelle Farrington <farringtond774@students.cochise.edu>
2020-12-11 13:51:52 -0700

    got unlockable menue working, all buttons now go to TestLoadRoom with correct song, and unlock next button

--- Assets/SongInfo.cs
+++ Assets/SongInfo.cs
@@ -6,11 +6,42 @@ using UnityEngine;
 public class SongInfo : ScriptableObject
 {
     [SerializeField]
-    private string songName;    
+    private string songName;
 
+    public bool stringsOn = false;
+    public bool pluckedOn = false;
+    public bool brassOn = false;
+    public bool windOn = false;
+    public bool keyboardOn = false;
+    public bool vocalOn = false;
     public string SongName
     {
         get { return songName; }
         set { songName = value; }
+    }
+
+    public void SetStringsOn()
+    {
+        stringsOn = true;
+    }
+    public void SetPluckedOn()
+    {
+        pluckedOn = true;
+    }
+    public void SetBrassOn()
+    {
+        brassOn = true;
+    }
+    public void SetWindOn()
+    {
+        windOn = true;
+    }
+    public void SetKeyboardOn()
+    {
+        keyboardOn = true;
+    }
+    public void SetVocalOn()
+    {
+        vocalOn = true;
     }
 }
--- Assets/SongMenuManager.cs
+++ Assets/SongMenuManager.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class SongMenuManager : MonoBehaviour
 {
-
+    [SerializeField]
+    private SongInfo globalSongInfo;
     [SerializeField]
     Button Per_PractButton;
     [SerializeField]
@@ -26,7 +27,30 @@ public class SongMenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(globalSongInfo.stringsOn == true)
+        {
+            Str_PractButton.interactable = true;
+        }
+        if (globalSongInfo.pluckedOn == true)
+        {
+            Plu_PractButton.interactable = true;
+        }
+        if (globalSongInfo.brassOn == true)
+        {
+            Bra_PractButton.interactable = true;
+        }
+        if (globalSongInfo.windOn == true)
+        {
+            Win_PractButton.interactable = true;
+        }
+        if (globalSongInfo.keyboardOn == true)
+        {
+            Key_PractButton.interactable = true;
+        }
+        if (globalSongInfo.vocalOn == true)
+        {
+            Voc_PractButton.interactable = true;
+        }
     }
 
     // Update is called once per frame
